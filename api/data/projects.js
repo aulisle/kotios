a456626289504a1d932921919b089868aa5a0f20
AulisL <aulis.leinonen@prezella.com>
2019-10-04 12:21:25 +0300

    Added saraco projects

--- api/data/projects.js
+++ api/data/projects.js
@@ -7,11 +7,11 @@ export const a = [
     },
     projectType: 2,
     title: 'Sompasaarenlaituri 12',
-    address: 'Sompasaarenlaituri 12, Helsinki',
-    tagline: 'Firan konsulttivetoinen hanke',
+    address: 'Sompasaarenlaituri 12, Kalasatama, Helsinki',
+    tagline: 'Konsulttivetoinen osallistava asuntohanke',
     values: ['resources'],
     phases: {
-      project: 4,
+      project: 2,
       plot: 2,
       group: 3
     },
@@ -19,16 +19,21 @@ export const a = [
       'https://www.fira.fi/wp-content/uploads/2019/06/SSL-julkisivu-merelta-800.jpg',
     images: [
       'https://www.fira.fi/wp-content/uploads/2019/06/SSL-julkisivu-merelta-800.jpg',
-      'https://www.fira.fi/wp-content/uploads/2017/12/sompasaarenlaituri-12-alue.jpg'
+      'https://www.fira.fi/wp-content/uploads/2017/12/sompasaarenlaituri-12-alue.jpg',
+      'https://www.saraco.fi/wp-content/uploads/2017/02/Kala-2_sis%C3%A4kuva1_4_4-1024x725.jpg'
+    ],
+    description: [
+      'Helsingin Albatrossiksi nimetyn asunto-osakeyhtiön kaikki asunnot on nyt varattu. Sompasaarenlaituri 8:ssa sijaitsevan kohteen maanrakennustyöt on jo aloitettu ja keväällä 2019 tontille alkaa nousta upouusi asuinkerrostalo.',
+      'Saracon ryhmärakennuttamishankkeena toteutettava talo tulee sijaitsemaan osoitteensa mukaisesti aivan rannan tuntumassa, ja useimmista asunnoista on näköala merelle. Kerrostaloon on suunniteltu yhteensä 47 laatuhuoneistoa, jotka on suunniteltu tulevien asukkaidensa tarpeiden mukaisiksi.'
     ],
     events: [],
     contacts: [
       {
-        name: 'V-P Rajala',
+        name: 'Rauni Kolehmainen',
         role: 'project-advisor',
-        email: 'email@email.com',
-        phone: '040 1234567',
-        initials: 'VR'
+        email: 'etunimi.sukunimi@saraco.fi',
+        phone: '+358 40 703 1828',
+        initials: 'RK'
       }
     ]
   },
@@ -262,6 +267,167 @@ export const a = [
     socialLiving: 0,
     housingAssociation: 'Koti kaupungissa ry',
     neighbourhood: 'torppala'
+  },
+
+  // Casa urbana
+  {
+    id: 7,
+    location: {
+      lat: 60.156598,
+      lng: 24.911837
+    },
+    title: 'Casa Urbana',
+    projectType: 4,
+    address: 'Järkäsaari, Helsinki',
+    tagline:
+      'Konsulttialoitteinen Hitas-kerrostalohanke Helsingin Jätkäsaaressa.',
+    description: [
+      'Casa Urbana on konsulttialoitteisena ryhmärakennuttamishankkeena toteutettava Hitas-kerrostalohanke Helsingin Jätkäsaaressa.',
+      'Casa Urbana –hankkeen ryhmärakennuttamiskonsulttina toimii R-Rakennuttajat Oy. Yrityksellä on Suomen rakennusmarkkinoilla poikkeuksellista osaamista ryhmärakennuttamishankkeen johtamisessa.'
+    ],
+    phases: {
+      project: 3,
+      plot: 2,
+      group: 3
+    },
+    values: ['5'],
+    thumbnail: '/casa-urbana/header.jpg',
+    images: ['/casa-urbana/header.jpg', '/casa-urbana/piha.jpg'],
+    events: [],
+    contacts: [
+      {
+        name: 'R-rakennuttajat Oy',
+        role: 'project-advisor',
+        email: ' ryhmarakennuttajat@gmail.com',
+        phone: '+358 50 371 2972',
+        initials: 'RR'
+      }
+    ]
+  },
+
+  // Saraco Kruunuvuorenranta
+  {
+    id: 8,
+    location: {
+      lat: 60.16772,
+      lng: 24.936071
+    },
+    title: 'Romeo ja Julia',
+    projectType: 4,
+    address: 'Pojamankatu 6, Kruunuvuorenranta, Helsinki',
+    tagline: 'Uniikkia asumista Kruunuvuorenrannassa',
+    description: [
+      'Romeo ja Julia ovat Saracon ryhmärakennuttamishankkeita, jotka tarjoavat ainutlaatuisen tilaisuuden ostaa uniikki koti uudenlaisella tavalla. Koon lisäksi voit valita asuntosi sijainnin talosta, ilmansuunnan, kerroksen sekä huoneluvun. Yhdessä arkkitehdin ja muiden asiantuntijoiden kanssa suunnittelet unelmakotisi jo ennen kuin rakennustyöt alkavat. Talojen yksiöt myydään ensisijaisesti isompien asuntojen laajennusosiksi, joten ne tulevat myyntiin viimeisenä.',
+      'Romeo ja Julia koostuvat matalista rivitalo-osista, sekä kerrostaloista. Kerrostalojen asukkaat saavat ihailla rauhoittavia merimaisemia lasitetuilta parvekkeiltaan. Matalissa rivitaloissa asukkaat puolestaan nauttivat oman pihan yksityisyydestä.  Oletko intohimoinen kokkaaja? Teetkö töitä kotona? Otetaan se huomioon kotisi suunnittelussa ja rakennutetaan yhdessä koti juuri sinulle.',
+      'Kohteet valmistuvat Kruunuvuorenrantaan vuoronperään: ensin rakennetaan Julia osoitteeseen Pojamankatu 6. Romeo nousee myöhemmin viereiselle tontille osoitteeseen Turumankatu 7.'
+    ],
+    phases: {
+      project: 0,
+      plot: 2,
+      group: 2
+    },
+    values: ['5'],
+    thumbnail: '/saraco/romeo-julia-thumbnail.jpg',
+    images: ['/saraco/romeo-julia-thumbnail.jpg'],
+    events: [],
+    contacts: [
+      {
+        name: 'Rauni Kolehmainen',
+        role: 'project-advisor',
+        email: 'etunimi.sukunimi@saraco.fi',
+        phone: '+358 40 703 1828',
+        initials: 'RK'
+      }
+    ]
+  },
+
+  // Saraco Polariksenkatu
+  {
+    id: 9,
+    location: {
+      lat: 60.184262,
+      lng: 24.98003
+    },
+    title: 'Polariksenkatu 9',
+    projectType: 4,
+    address: 'Pojamankatu 6, Kruunuvuorenranta, Helsinki',
+    tagline: 'Uusi kaupunkikoti meren äärellä',
+    description: [
+      'Merenrantaan rajautuvaan Kipparin kortteliin nousee ryhmärakennuttamishankkeena nelikerroksinen pienkerrostalo, johon valmistuu yhteensä 18 korkealaatuista uudisasuntoa. Jokainen koti tulee vastaamaan asukkaidensa toiveita pohjaratkaisua ja materiaaleja myöten.',
+      'Yhteisöllisyyttä arkeen tuovat talon monipuoliset yhteistilat sekä Kalasataman viihtyisät korttelipihat, joiden ansiosta kodin tunnelma laajenee oman asunnon ulkopuolelta naapurustoon saakka.'
+    ],
+    phases: {
+      project: 4,
+      plot: 2,
+      group: 3
+    },
+    values: ['5'],
+    thumbnail:
+      'https://www.saraco.fi/wp-content/uploads/2017/01/2019-09-30-Saraco-Valmiit-Kohteet-18-1333x900.jpg',
+    images: [
+      'https://www.saraco.fi/wp-content/uploads/2017/01/2019-09-30-Saraco-Valmiit-Kohteet-18-1333x900.jpg',
+      'https://www.saraco.fi/wp-content/uploads/2017/01/2019-09-30-Saraco-Valmiit-Kohteet-13-1-1024x682.jpg',
+      'https://www.saraco.fi/wp-content/uploads/2017/01/2019-09-30-Saraco-Valmiit-Kohteet-14-682x1024.jpg',
+      'https://www.saraco.fi/wp-content/uploads/2017/01/2019-09-30-Saraco-Valmiit-Kohteet-19-682x1024.jpg',
+      'https://www.saraco.fi/wp-content/uploads/2017/01/2019-09-30-Saraco-Valmiit-Kohteet-11-682x1024.jpg',
+      'https://www.saraco.fi/wp-content/uploads/2017/01/2019-09-30-Saraco-Valmiit-Kohteet-9-682x1024.jpg',
+      'https://www.saraco.fi/wp-content/uploads/2017/01/keittio%CC%884-1024x734.jpg',
+      'https://www.saraco.fi/wp-content/uploads/2017/01/kylpyhuone3-1024x683.jpg',
+      'https://www.saraco.fi/wp-content/uploads/2017/01/kylpyhuone9-1-1024x683.jpg'
+    ],
+    events: [],
+    contacts: [
+      {
+        name: 'Rauni Kolehmainen',
+        role: 'project-advisor',
+        email: 'etunimi.sukunimi@saraco.fi',
+        phone: '+358 40 703 1828',
+        initials: 'RK'
+      }
+    ]
+  },
+
+  // Saraco Juutinraumankatu 6
+  {
+    id: 10,
+    location: {
+      lat: 60.159067,
+      lng: 24.919417
+    },
+    title: 'Juutinraumankatu 6',
+    projectType: 4,
+    address: 'Juutinraumankatu 6, Jätkäsaari, Helsinki',
+    tagline: 'Saracon ensimmäinen ryhmärakennuttamiskohde',
+    description: [
+      'Kaksiportaisen asuinkerrostalon sekä kolmikerroksisen townhouse-osan kaikki asunnot ovat asukkaiden tarpeiden mukaisiksi suunniteltuja, yksilöllisiä koteja. Kerrostalohuoneistojen ja townhouse-osan muodostamaan kokonaisuuteen valmistui yhteensä 45 asuntoa. Kooltaan Juutinrauma 6:n kohteeseen rakennetut asunnot vaihtelivat yksiöstä 120 m²:n kokoisiin koteihin. Kohteen räätälöityihin ratkaisuihin lukeutuu muun muassa kattoterassille asennettu ulkoporeallas.',
+      'Kohteen suunnittelusta vastasi Anttinen Oiva Arkkitehdit, projektiarkkitehteina toimivat Matti Huhtamies ja Sanna Meriläinen.'
+    ],
+    phases: {
+      project: 4,
+      plot: 2,
+      group: 3
+    },
+    values: ['5'],
+    thumbnail:
+      'https://www.saraco.fi/wp-content/uploads/2017/01/2019-09-30-Saraco-Valmiit-Kohteet-1-1600x900.jpg',
+    images: [
+      'https://www.saraco.fi/wp-content/uploads/2017/01/2019-09-30-Saraco-Valmiit-Kohteet-1-1024x682.jpg',
+      'https://www.saraco.fi/wp-content/uploads/2017/01/2019-09-30-Saraco-Valmiit-Kohteet-3-1024x682.jpg',
+      'https://www.saraco.fi/wp-content/uploads/2017/01/2019-09-30-Saraco-Valmiit-Kohteet-5-1024x682.jpg',
+      'https://www.saraco.fi/wp-content/uploads/2017/02/Huoneisto-1-1024x683.jpg',
+      'https://www.saraco.fi/wp-content/uploads/2017/02/Huoneisto-11-1024x683.jpg',
+      'https://www.saraco.fi/wp-content/uploads/2017/02/Huoneisto-9-1024x683.jpg'
+    ],
+    events: [],
+    contacts: [
+      {
+        name: 'Rauni Kolehmainen',
+        role: 'project-advisor',
+        email: 'etunimi.sukunimi@saraco.fi',
+        phone: '+358 40 703 1828',
+        initials: 'RK'
+      }
+    ]
   }
 ]
 
